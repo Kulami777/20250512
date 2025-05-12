bd3e8020d1f0672eec491d3991b9638b97cfa79a
Kulami777 <singying941213@gmail.com>
2025-05-12 11:01:25 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,6 +9,10 @@ const leftEyePoints = [
   243, 190, 56, 28, 27, 29, 30, 247, 130, 25, 110, 24, 23, 22, 26, 112,
   133, 173, 157, 158, 159, 160, 161, 246, 33, 7, 163, 144, 145, 153, 154, 155
 ];
+const rightEyePoints = [
+  359, 467, 260, 259, 257, 258, 286, 444, 463, 341, 256, 252, 253, 254, 339, 255,
+  263, 466, 388, 387, 386, 385, 384, 398, 362, 382, 381, 380, 374, 373, 390, 249
+];
 
 function setup() {
   createCanvas(640, 480);
@@ -41,6 +45,9 @@ function draw() {
 
     // 繪製左眼連線
     drawConnections(keypoints, leftEyePoints, color(0, 0, 255)); // 藍色線條
+
+    // 繪製右眼連線
+    drawConnections(keypoints, rightEyePoints, color(0, 255, 0)); // 綠色線條
   }
 }
 
